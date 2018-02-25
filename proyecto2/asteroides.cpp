aabf5a661178f4b95fb5ca929feb820496f4b47c
Tonatihu <carlostonatihu@gmail.com>
2018-02-25 17:17:34 -0600

    Poniendo el codigo de la clase asteroide mas bonito :rocket:

--- proyecto2/asteroides.cpp
+++ proyecto2/asteroides.cpp
@@ -6,75 +6,102 @@
 #include <vector>
 #include <iostream>
 #include <math.h>
+
 #define PI 3.14159265
 
-#define RADIO_MAXIMO 80
+#define RADIO_MAXIMO 100
 #define RADIO_MINIMO 10
+
+#define MIN_NUM_LADOS 5
+
+#define LARGO_VENTANA 800
+#define ANCHO_VENTANA 495
+
+#define MAX_VEL_ANGULAR 30
+#define MIN_VEL_ANGULAR 5
+
+#define MAX_VELOCIDAD 10
+#define MIN_VELOCIDAD 1
+
 using namespace std;
 
 class Asteroide {
-public:
-    int num_lados; // Numero de lineas del asteroide al menos 5 lados
+private:
+    int num_lados;
     double angulo;
-    //int direccion_x;
-    //int direccion_y;
-    int centro_x;
-    int centro_y;
+    double vel_angular;
+    double velocidad;
+    int *direccion = new int[2]();
+    int *centro = new int[2]();
     double radio;
-    vector<vector<int>> lados;
+    vector<vector<int>> vertices;
     vector<vector<int>> vectores;
-    vector<vector <double>> matriz_angulos;
+    vector<vector <double>> matriz_rotacion;
 
-    Asteroide();
+public:
+    Asteroide(void);
 
     Asteroide(int numero, int x, int y, double radio_asteroide){
         num_lados = numero;
-        centro_x = x;
-        centro_y = y;
-        radio = radio_asteroide + 0.0;
+        angulo = 360.0/num_lados;
+        centro[0] = x;
+        centro[1] = y;
+        radio = radio_asteroide;
 
         vectores.resize(num_lados);
-        lados.resize(num_lados);
+        vertices.resize(num_lados);
         for (int i = 0; i < num_lados; i++){
-            lados[i].resize(2);
+            vertices[i].resize(2);
             vectores[i].resize(2);
         }
 
-        angulo = (double) 360.0/num_lados;
-        cout << "Lados: "  << num_lados << endl;
-        cout << "Angulo: " << angulo << endl;
+        velocidad = (-0.1*radio) + 11;
 
-        matriz_angulos.resize(2);
-        matriz_angulos[0].resize(2);
-        matriz_angulos[1].resize(2);
+        direccion[0] = rand() % (MAX_VELOCIDAD - MIN_VELOCIDAD + 1) + MIN_VELOCIDAD;
+        direccion[1] = (velocidad*velocidad) - (direccion[0]*direccion[0]);
+        direccion[1] = sqrt(direccion[1]);
 
-        matriz_angulos[0][0] = cos(10*PI/180);
-        matriz_angulos[0][1] = -sin(10*PI/180);
-        matriz_angulos[1][0] = sin(10*PI/180);
-        matriz_angulos[1][1] = cos(10*PI/180);
+        if (rand() % 2)
+            direccion[0] *= -1;
+        if (rand() % 2)
+            direccion[0] *= -1;
+
+        vel_angular = (-0.21*radio) + 22.14;
+        if (rand() % 2)
+            vel_angular *= -1;
+
+        inicializar_matriz_rotacion();
     }
 
-    void dibujar() {
-        calcular_coordenadas();
-        for (int i = 0; i < num_lados; i++){
-            lados[i][0] = vectores[i][0] + centro_x;
-            lados[i][1] = vectores[i][1] + centro_y;
-        }
+    void inicializar_matriz_rotacion(void) {
+        matriz_rotacion.resize(2);
+        matriz_rotacion[0].resize(2);
+        matriz_rotacion[1].resize(2);
 
-        for (int i = 0; i < num_lados-1; i++){
-            gfx_line(lados[i][0], lados[i][1], lados[i+1][0], lados[i+1][1]);
-            //gfx_line(centro_x, centro_y, lados[i][0], lados[i][1]);
-        }
+        matriz_rotacion[0][0] = cos(vel_angular*PI/180);
+        matriz_rotacion[0][1] = -sin(vel_angular*PI/180);
+        matriz_rotacion[1][0] = sin(vel_angular*PI/180);
+        matriz_rotacion[1][1] = cos(vel_angular*PI/180);
 
-        gfx_line(lados[0][0], lados[0][1], lados[num_lados-1][0], lados[num_lados-1][1]);
+        return;
     }
 
-    void calcular_coordenadas() {
+    void dibujar(void) {
+        int i = 0;
+        for (; i < num_lados-1; i++)
+            gfx_line(vertices[i][0], vertices[i][1], vertices[i+1][0], vertices[i+1][1]);
+        
+        gfx_line(vertices[0][0], vertices[0][1], vertices[i][0], vertices[i][1]);
+
+        return;
+    }
+
+    void calcular_coordenadas(void) {
         int *valores_y = new int[2]();
         int *valores_x = new int[2]();
 
-        int magnitud_A = 0;
-        int magnitud_B = 0;
+        int magnitud_A = radio;
+        int magnitud_B = magnitud_A;
         
         int coord_a = 0;
         int coord_b = 0;
@@ -85,8 +112,6 @@ public:
 
         bool comparacion = false;
 
-        magnitud_A = rand() % (int)((radio+10) - (radio) + 1) + (radio);
-        magnitud_B = magnitud_A;
         for (int i = 0; i < num_lados-1; i++) {
             //magnitud_B = magnitud_A;
             if (i == 0) {
@@ -103,17 +128,15 @@ public:
                 if (coord_a == 0)
                     return;
 
-                a = ((coord_b * coord_b) / (coord_a * coord_a)) + 1;
-                b = -(2 * magnitud_A * magnitud_B * coord_b * cos(angulo * PI / 180.0))/(coord_a * coord_a);
-                c = magnitud_A * magnitud_B * cos(angulo * PI / 180.0) / coord_a;
-                c = c * c;
-                c = c - (magnitud_B * magnitud_B);
+                a = ((coord_b*coord_b)/(coord_a*coord_a)) + 1;
+                b = -(2*magnitud_A*magnitud_B*coord_b*cos(angulo*PI/180.0))/(coord_a*coord_a);
+                c = magnitud_A*magnitud_B*cos(angulo*PI/180.0)/coord_a;
+                c = c*c;
+                c = c - (magnitud_B*magnitud_B);
 
                 // Mas errores
                 if (a == 0)
                     return;
-                cout << "a="<< a << " b=" << b << " c="<< c << endl;
-                cout << "\n";
 
                 resolver_trinomio(a, b, c, valores_y);
 
@@ -135,31 +158,36 @@ public:
                 vectores[i+1][1] = valores_y[1];
             }
 
-            //magnitud_A = magnitud_B;
         }
+
+        for (int i = 0; i < num_lados; i++){
+            vertices[i][0] = vectores[i][0] + centro[0];
+            vertices[i][1] = vectores[i][1] + centro[1];
+        }
+
+        return;
     }
 
-    void resolver_trinomio(float a, float b, float c, int *valores_y) {
+    void resolver_trinomio(float a, float b, float c, int *resultados) {
         double dentro = b*b - 4*a*c;
-        double raiz = sqrt(dentro);
-        int resultado1 = (-b + raiz) / (2*a);
-        int resultado2 = (-b - raiz) / (2*a);
-        valores_y[0] = resultado1;
-        valores_y[1] = resultado2;
+        dentro = sqrt(dentro);
+        resultados[0] = (-b + dentro)/(2*a);
+        resultados[1] = (-b - dentro)/(2*a);
+
+        return;
     }
 
-    bool comparar_distancias(int centro_x, int centro_y, int x1, int y1, int x2, int y2) {
-        double c1 = ((centro_x-x1) * (centro_x-x1)) + ((centro_y-y1) * (centro_y-y1));
-        double c2 = ((centro_x-x2) * (centro_x-x2)) + ((centro_y-y2) * (centro_y-y2));
+    bool comparar_distancias(int x, int y, int x1, int y1, int x2, int y2) {
+        double c1 = ((x - x1)*(x - x1)) + ((y - y1)*(y - y1));
+        double c2 = ((x - x2)*(x - x2)) + ((y - y2)*(y - y2));
 
         if (c1 > c2)
             return true;
         return false;
     }
 
-    void rotar() {
+    void rotar(void) {
         vector<vector <double>> resta;
-        
         vector<vector <double>> nuevos_vertices;
 
         resta.resize(num_lados);
@@ -169,51 +197,51 @@ public:
             resta[i].resize(2);
             nuevos_vertices[i].resize(2);
 
-            resta[i][0] = lados[i][0] - centro_x;
-            resta[i][1] = lados[i][1] - centro_y;
+            resta[i][0] = vertices[i][0] - centro[0];
+            resta[i][1] = vertices[i][1] - centro[1];
         }
 
-        for (int i = 0; i < num_lados; i++) {
+        for (int i = 0; i < num_lados; i++)
             for (int j = 0; j < 2; j++) {
                 nuevos_vertices[i][j] = 0;
-                for (int k = 0; k < 2; k++) {
-                    nuevos_vertices[i][j] += matriz_angulos[j][k] * resta[i][k];
-                }
+                for (int k = 0; k < 2; k++)
+                    nuevos_vertices[i][j] += matriz_rotacion[j][k] * resta[i][k];
             }
-        }
 
         for (int i = 0; i < num_lados; i++){
-            lados[i][0] = round(nuevos_vertices[i][0] + centro_x);
-            lados[i][1] = round(nuevos_vertices[i][1] + centro_y);
-        }
-        //cout << "vertice: " << lados[0][0] << ", "<< lados[0][1]<< endl;
-
-
-        
-        for (int i = 0; i < num_lados-1; i++){
-            gfx_line(lados[i][0], lados[i][1], lados[i+1][0], lados[i+1][1]);
-            //gfx_line(centro_x, centro_y, lados[i][0], lados[i][1]);
+            vertices[i][0] = round(nuevos_vertices[i][0] + centro[0]);
+            vertices[i][1] = round(nuevos_vertices[i][1] + centro[1]);
         }
 
-        gfx_line(lados[0][0], lados[0][1], lados[num_lados-1][0], lados[num_lados-1][1]);
-        
+        dibujar();
+
+        return;
     }
 
-    // Mueve el asteroide con base en su radio (tamanio) y la direccion y centro
-    void mover_asterioide();
+    void mover(void) {
+        centro[0] += direccion[0];
+        centro[1] += direccion[1];
+
+        for (int i = 0; i < num_lados; i++) {
+            vertices[i][0] += direccion[0];
+            vertices[i][1] += direccion[1];
+        }
+
+        dibujar();
+
+        return;
+    }
 };
-
-
 
 int main(int argc, char const *argv[]) {
     srand (time(NULL));
-    gfx_open(800, 600, "Animacion de asteroides");
+    gfx_open(LARGO_VENTANA, ANCHO_VENTANA, "Animacion de asteroides");
     gfx_color(0, 200, 100);
     gfx_clear();
 
     vector<Asteroide> asteroides;
 
-    //asteroides.resize(5);
+    asteroides.reserve(5);
 
     int radio_random = 10;
     int posicion_x = 0;
@@ -224,16 +252,24 @@ int main(int argc, char const *argv[]) {
     for (int i = 0; i < 5; ++i){
         radio_random = rand() % (RADIO_MAXIMO-RADIO_MINIMO + 1) + RADIO_MINIMO;
         lados = rand() % 3 + 5;
-        posicion_y = rand() % (400-20 + 1) + 20;
-        posicion_x = rand() % (800-20 + 1) + 20;
+        posicion_y = rand() % (ANCHO_VENTANA-50 + 1) + 50;
+        posicion_x = rand() % (LARGO_VENTANA-50 + 1) + 50;
         Asteroide aux(lados, posicion_x, posicion_y, radio_random);
         asteroides.push_back(aux);
+        asteroides[i].calcular_coordenadas();
         asteroides[i].dibujar();
     }
 
     gfx_flush();
-    usleep(100000);
-    for(int t = 0; t < 25; t++){
+    usleep(50000);
+    for(int t = 0; t < 40; t++){
+        gfx_clear();
+        asteroides[0].mover();
+        asteroides[1].mover();
+        asteroides[2].mover();
+        asteroides[3].mover();
+        asteroides[4].mover();
+
         gfx_clear();
         asteroides[0].rotar();
         asteroides[1].rotar();
@@ -242,7 +278,7 @@ int main(int argc, char const *argv[]) {
         asteroides[4].rotar();
 
         gfx_flush();
-        usleep(100000); //24 por segundo
+        usleep(50000); //24 por segundo
     }
     /*
     char c;
